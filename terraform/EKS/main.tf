85653e80c1d63aaabf43a1fa9e52afbcfeb3b1a1
AShakerQA <AShakerQA.com>
2020-11-21 18:10:19 +0000

    configuring eks node

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -1,10 +1,25 @@
 resource "aws_eks_cluster" "eks" {
   name     = "example"
-  role_arn = "arn:aws:iam::973553080645:role/cluster_role"
+  role_arn = "var.arnrole"
 
   vpc_config {
     subnet_ids       = [var.sub1, var.sub2]
     subnet_group_ids = [var.security_id]
   }
+
+resource "aws_eks_node_group" "eks_node" {
+  cluster_name    = "eks cluster node"
+  node_group_name = "default"
+  node_role_arn   = var.arnnode
+  subnet_ids      = [var.sub1, var.sub2]
+  instance_types = "t3.medium"
+  ami_type = "AL2_x86_64
+"
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
 }
 
--- terraform/EKS/variables.tf
+++ terraform/EKS/variables.tf
@@ -9,3 +9,11 @@ variable "sub2" {
 variable "security_id" {
   description = "security group id"
 }
+
+variable "arnroles"{
+  default = "arn:aws:iam::973553080645:role/cluster_role"
+}
+
+variable "arnnode" {
+  default = "arn:aws:iam::973553080645:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
+}
