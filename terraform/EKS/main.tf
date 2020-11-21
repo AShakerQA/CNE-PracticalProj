c4d0f7a017327fdd8ab4d6cd78cb31c6d7bf0119
AShakerQA <AShakerQA.com>
2020-11-21 18:36:28 +0000

    updated roles

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -7,19 +7,19 @@ resource "aws_eks_cluster" "eks" {
     subnet_group_ids = [var.security_id]
   }
 
-resource "aws_eks_node_group" "eks_node" {
-  cluster_name    = "eks cluster node"
-  node_group_name = "default"
-  node_role_arn   = var.arnnode
-  subnet_ids      = [var.sub1, var.sub2]
-  instance_types = "t3.medium"
-  ami_type = "AL2_x86_64"
+  resource "aws_eks_node_group" "eks_node" {
+    cluster_name    = "eks cluster node"
+    node_group_name = "default"
+    node_role_arn   = var.arnnode
+    subnet_ids      = [var.sub1, var.sub2]
+    instance_types  = "t3.medium"
+    ami_type        = "AL2_x86_64"
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 2
-  }
+    scaling_config {
+      desired_size = 2
+      max_size     = 2
+      min_size     = 2
+    }
 
   }
 }
--- terraform/EKS/variables.tf
+++ terraform/EKS/variables.tf
@@ -10,10 +10,10 @@ variable "security_id" {
   description = "security group id"
 }
 
-variable "arnroles"{
+variable "arnroles" {
   default = "arn:aws:iam::973553080645:role/cluster_role"
 }
 
 variable "arnnode" {
-  default = "arn:aws:iam::973553080645:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
+  default = "arn:aws:iam::973553080645:role/NodeInstanceRole"
 }
