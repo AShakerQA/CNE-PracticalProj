d1bab0721a06b0a889dcab4aa382083e09e695b3
AShakerQA <AShakerQA.com>
2020-11-21 17:34:25 +0000

    creating eks cluster

---
+++ terraform/EKS/outputs.tf
@@ -0,0 +1,6 @@
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
---
+++ terraform/EKS/variables.tf
@@ -0,0 +1,11 @@
+variable "sub1" {
+  description = "subnet id from vpc"
+}
+
+variable "sub2" {
+  description = "subnet id from vpc"
+}
+
+variable "security_id" {
+  description = "security group id"
+}
