3065d471d1214197b3c88da6c65eacd1d39a8c35
AShakerQA <AShaker@qa.com>
2020-11-22 15:56:27 +0000

    mod

--- utility.sh
+++ utility.sh
@@ -13,6 +13,15 @@ git clone https://github.com/AShakerQA/CNE-PracticalProj.git
 cd CNE-PracticalProj
 git checkout jenkins
 
+sudo apt-get update
+        sudo apt install mysql-client-core-5.7
+
+
+mysql -h terraform-20201122151346712900000002.coaea37d1emt.eu-west-1.rds.amazonaws.com -P 3306 -u admin -pPassword1234
+use testdb;
+source ~/CNE-PracticalProj/database/Create.sql;
+exit
+
 sudo docker-compose up -d
 sudo docker exec backend bash -c "pytest tests/ --cov application"
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
